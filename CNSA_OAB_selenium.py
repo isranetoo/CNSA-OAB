e4e94a36bec78bcd6deee9c20de4b59eab13971e
Israel Antunes Neto <62219211+isranetoo@users.noreply.github.com>
2024-12-17 14:54:48 -0300

    v4

---
+++ CNSA_OAB_selenium.py
@@ -0,0 +1,71 @@
+import os
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+
+URL_BASE = "https://cna.oab.org.br"
+PASTA_ENTRADA = "resultados_CNA_detalhes"
+PASTA_SAIDA = "resultados_CNSA_detalhes"
+
+os.makedirs(PASTA_ENTRADA, exist_ok=True)
+os.makedirs(PASTA_SAIDA, exist_ok=True)
+
+def iniciar_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") 
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    return driver
+
+def processar_arquivo():
+    driver = iniciar_driver()
+    for arquivo in os.listdir(PASTA_ENTRADA):
+        if arquivo.endswith(".json"):
+            caminho_arquivo = os.path.join(PASTA_ENTRADA, arquivo)
+            with open(caminho_arquivo, 'r', encoding='utf-8') as f:
+                dados = json.load(f)
+            sociedades = dados.get("Data", {}).get("Sociedades", [])
+            if not sociedades:
+                print(f"Nenhuma sociedade encotrada no arquivo: {arquivo}")
+                resultado = {
+                    "erro": "Nenhuma sociedade encotrada",
+                    "url": None
+                }
+            else:
+                url_parcial = sociedades[0].get("Url")
+                if url_parcial:
+                    url_completa = URL_BASE + url_parcial
+                    print(f"Acessando a URL: {url_completa}")
+                    try:
+                        driver.get(url_completa)
+                        url_final = driver.current_url
+                        resultado = {
+                            "conteudo": arquivo,
+                            "url": url_final
+                        }
+                    except Exception as e:
+                        print(f"Erro ao acessar a página: {e}")
+                        resultado = {
+                            "erro": str(e),
+                            "url": url_completa
+                        }
+                else:
+                    print(f"URL não encotrada no arquivo: {arquivo}")
+                    resultado = {
+                        "erro": "URL não encotrada",
+                        "url": None
+                    }
+            caminho_saida = os.path.join(PASTA_SAIDA, arquivo)
+            with open(caminho_saida, 'w', encoding='utf-8') as f_out:
+                json.dump(resultado, f_out, ensure_ascii=False, indent=4)
+
+            print(f"Arquivo processado e salvo em: {caminho_saida}")
+
+    driver.quit()
+
+if __name__ == "__main__":
+    processar_arquivo()
+
